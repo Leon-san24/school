c8736c8c4ed389740bcc8c1660cb8fa6cbc6ea75
Leon-san24 <leonblyum@gmail.com>
2022-09-13 14:46:57 +0200

    Amen 4hoiözlökzpizi

---
+++ stack.py
@@ -0,0 +1,57 @@
+
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next_node = None
+
+    def get_content(self):
+        return self.value
+
+    def set_content(self, pContent):
+        self.value = pContent
+
+    def set_next(self, pNext):
+        self.next_node = pNext
+        return self.next_node
+
+
+class Stack:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self):
+        name = input("Name: ")
+
+        new_node = Node(name)
+        if self.is_empty():
+            self.head = new_node
+        else:
+            new_node.set_next(self.head)
+            self.head = new_node
+         
+    def dequeue(self):
+        if self.is_empty() == False:
+            self.head = self.head.next_node
+            print("Dequeded!")
+        else:
+            return None
+
+    def top(self):
+        if self.is_empty() is None:
+            return
+        else:
+            return self.head
+
+    def is_empty(self):
+        return self.head is None
+
+newStack = Stack()
+
+for i in range(3):
+    newStack.push()
+
+newStack.dequeue()
+print(newStack.top().value)
